60e8f3a5a7e97753b1364ad250c868dababaa607
Dominik Pinsel <dominik.pinsel@daimler.com>
2022-08-05 07:22:52 +0200

    new tests for DataPlaneSelectorConfigurationServiceExtension
    Signed-off-by: Dominik Pinsel <dominik.pinsel@daimler.com>

--- edc-extensions/dataplane-selector-configuration/src/test/java/net/catenax/edc/dataplane/selector/configuration/DataPlaneSelectorConfigurationServiceExtensionTest.java
+++ edc-extensions/dataplane-selector-configuration/src/test/java/net/catenax/edc/dataplane/selector/configuration/DataPlaneSelectorConfigurationServiceExtensionTest.java
@@ -16,14 +16,22 @@ package net.catenax.edc.dataplane.selector.configuration;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.eclipse.dataspaceconnector.dataplane.selector.DataPlaneSelectorService;
+import org.eclipse.dataspaceconnector.spi.EdcException;
 import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtensionContext;
 import org.eclipse.dataspaceconnector.spi.system.configuration.Config;
 import org.eclipse.dataspaceconnector.spi.system.configuration.ConfigFactory;
 import org.eclipse.dataspaceconnector.spi.types.domain.DataAddress;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.mockito.Mockito;
 
 class DataPlaneSelectorConfigurationServiceExtensionTest {
@@ -37,27 +45,46 @@ class DataPlaneSelectorConfigurationServiceExtensionTest {
       String.format("%s, %s", S3_BUCKET, BLOB_STORAGE);
   private static final String DATA_PLANE_INSTANCE_DESTINATION_TYPES = LOCAL_FILE_SYSTEM;
 
-  private Map<String, String> getConfig() {
-    final String urlKey =
-        String.format(
-            "%s.%s",
-            DATA_PLANE_INSTANCE_ID, DataPlaneSelectorConfigurationServiceExtension.URL_SUFFIX);
-    final String sourceTypesKey =
-        String.format(
-            "%s.%s",
-            DATA_PLANE_INSTANCE_ID,
-            DataPlaneSelectorConfigurationServiceExtension.SOURCE_TYPES_SUFFIX);
-    final String destinationTypesKey =
-        String.format(
-            "%s.%s",
-            DATA_PLANE_INSTANCE_ID,
-            DataPlaneSelectorConfigurationServiceExtension.DESTINATION_TYPES_SUFFIX);
-    final String propertiesKey =
-        String.format(
-            "%s.%s",
-            DATA_PLANE_INSTANCE_ID,
-            DataPlaneSelectorConfigurationServiceExtension.PROPERTIES_SUFFIX);
+  private static final String urlKey =
+      String.format(
+          "%s.%s",
+          DATA_PLANE_INSTANCE_ID, DataPlaneSelectorConfigurationServiceExtension.URL_SUFFIX);
+  private static final String sourceTypesKey =
+      String.format(
+          "%s.%s",
+          DATA_PLANE_INSTANCE_ID,
+          DataPlaneSelectorConfigurationServiceExtension.SOURCE_TYPES_SUFFIX);
+  private static final String destinationTypesKey =
+      String.format(
+          "%s.%s",
+          DATA_PLANE_INSTANCE_ID,
+          DataPlaneSelectorConfigurationServiceExtension.DESTINATION_TYPES_SUFFIX);
+  private static final String propertiesKey =
+      String.format(
+          "%s.%s",
+          DATA_PLANE_INSTANCE_ID, DataPlaneSelectorConfigurationServiceExtension.PROPERTIES_SUFFIX);
 
+  private DataPlaneSelectorConfigurationServiceExtension extension;
+
+  // mocks
+  private ServiceExtensionContext serviceExtensionContext;
+  private DataPlaneSelectorService dataPlaneSelectorService;
+  private Monitor monitor;
+
+  @BeforeEach
+  void setup() {
+    extension = new DataPlaneSelectorConfigurationServiceExtension();
+
+    serviceExtensionContext = Mockito.mock(ServiceExtensionContext.class);
+    dataPlaneSelectorService = Mockito.mock(DataPlaneSelectorService.class);
+    monitor = Mockito.mock(Monitor.class);
+
+    Mockito.when(serviceExtensionContext.getService(DataPlaneSelectorService.class))
+        .thenReturn(dataPlaneSelectorService);
+    Mockito.when(serviceExtensionContext.getMonitor()).thenReturn(monitor);
+  }
+
+  private Map<String, String> getConfig() {
     return new HashMap<>() {
       {
         put(urlKey, DATA_PLANE_INSTANCE_URL);
@@ -84,27 +111,20 @@ class DataPlaneSelectorConfigurationServiceExtensionTest {
 
   @Test
   void testInitialize() {
-    final DataPlaneSelectorConfigurationServiceExtension extension =
-        new DataPlaneSelectorConfigurationServiceExtension();
 
-    final ServiceExtensionContext serviceExtensionContext =
-        Mockito.mock(ServiceExtensionContext.class);
-    final DataPlaneSelectorService dataPlaneSelectorService =
-        Mockito.mock(DataPlaneSelectorService.class);
-    final Monitor monitor = Mockito.mock(Monitor.class);
     final Config config = ConfigFactory.fromMap(getConfig());
 
-    Mockito.when(serviceExtensionContext.getService(DataPlaneSelectorService.class))
-        .thenReturn(dataPlaneSelectorService);
     Mockito.when(serviceExtensionContext.getConfig("edc.dataplane.selector")).thenReturn(config);
-    Mockito.when(serviceExtensionContext.getMonitor()).thenReturn(monitor);
 
     extension.initialize(serviceExtensionContext);
 
     Mockito.verify(serviceExtensionContext, Mockito.times(1))
         .getService(DataPlaneSelectorService.class);
     Mockito.verify(serviceExtensionContext, Mockito.times(1)).getMonitor();
-    Mockito.verify(serviceExtensionContext, Mockito.times(1)).getConfig("edc.dataplane.selector");
+    Mockito.when(
+            serviceExtensionContext.getConfig(
+                DataPlaneSelectorConfigurationServiceExtension.CONFIG_PREFIX))
+        .thenReturn(config);
 
     Mockito.verify(dataPlaneSelectorService, Mockito.times(1))
         .addInstance(
@@ -148,7 +168,51 @@ class DataPlaneSelectorConfigurationServiceExtensionTest {
                       && matchingCanHandleS3ToFileSystem
                       && matchingCanHandleBlobToFileSystem;
                 }));
+  }
 
-    Mockito.verifyNoMoreInteractions(serviceExtensionContext, dataPlaneSelectorService);
+  @ParameterizedTest
+  @ArgumentsSource(MissingConfigArgumentsProvider.class)
+  public void testWarningOnPropertyMissing(String configKey, String configValue) {
+    Map<String, String> configMap = getConfig();
+    configMap.put(configKey, configValue);
+
+    final Config config = ConfigFactory.fromMap(configMap);
+
+    Mockito.when(
+            serviceExtensionContext.getConfig(
+                DataPlaneSelectorConfigurationServiceExtension.CONFIG_PREFIX))
+        .thenReturn(config);
+
+    extension.initialize(serviceExtensionContext);
+
+    // one warning config missing, one warning data plane instance skipped
+    Mockito.verify(monitor, Mockito.times(2)).warning(Mockito.anyString());
+  }
+
+  @Test
+  public void throwsExceptionOnPropertiesNoJson() {
+    Map<String, String> configMap = getConfig();
+    configMap.put(propertiesKey, "no json");
+
+    final Config config = ConfigFactory.fromMap(configMap);
+
+    Mockito.when(
+            serviceExtensionContext.getConfig(
+                DataPlaneSelectorConfigurationServiceExtension.CONFIG_PREFIX))
+        .thenReturn(config);
+
+    Assertions.assertThrows(
+        EdcException.class, () -> extension.initialize(serviceExtensionContext));
+  }
+
+  private static class MissingConfigArgumentsProvider implements ArgumentsProvider {
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+      return Stream.of(
+          Arguments.of(urlKey, ""),
+          Arguments.of(sourceTypesKey, ""),
+          Arguments.of(destinationTypesKey, ""),
+          Arguments.of(propertiesKey, "{}"));
+    }
   }
 }
